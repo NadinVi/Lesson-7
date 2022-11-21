b13285f5dc3901dc7840fd4d1db0e74df50956ae
Family Computer <vinokhodova.nadin@gmail.com>
2022-11-21 19:43:00 +0100

    add task

--- 14/script.js
+++ 14/script.js
@@ -4,4 +4,12 @@
 // const array = [1, 2, 3, 4, 5, 6, 7];
 // removeElement(array, 5 );
 // console.log(array);
-// // Результат: [1, 2, 3, 4, 6, 7]
+// Результат: [1, 2, 3, 4, 6, 7]
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement() {
+    array.splice(4, 1);
+    console.log(array);
+}
+removeElement()

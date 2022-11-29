38f7f1c142ae698e4c2654bb6d2310128971e4ec
Family Computer <vinokhodova.nadin@gmail.com>
2022-11-29 19:20:13 +0100

    add cards

--- 13/script.js
+++ 13/script.js
@@ -11,3 +11,35 @@
 
 // Все теги рендерим с помощью js.
 // Внешний вид задания НЕ важен, важен только JS-код))
+
+
+cardSuits = ["clubs", "spades", "diamonds", "hearts"];
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+cards = [];
+
+
+for(i = 0; i < numbers.length; i++) {
+    for(k = 0; k < cardSuits.length; k++) {
+        cards.push(`
+    <div class="cards">
+        <div class="card_img">${numbers[i]} <img src="images/suits/${cardSuits[k]}.svg" alt="card number and suit"></div>
+       <div class="card_img">${numbers[i]} <img src="images/suits/${cardSuits[k]}.svg" alt="card number and suit"></div>
+    </div>`)
+    }
+}
+
+jaQueKiCards = ["J", "Q", "K"];
+jackQueenKing = ["jack", "queen", "king"];
+
+for(i = 0; i < jaQueKiCards.length; i++) {
+    for(k = 0; k < cardSuits.length; k++) {
+        cards.push(`
+    <div class="cards">
+        <div class="card_img">${jaQueKiCards[i]} <img src="images/suits/${cardSuits[k]}.svg" alt="card number and suit"></div>
+        <img class="jack_queen_king" src="images/images/${jackQueenKing[i]}.svg" alt="card person and suit">
+       <div class="card_img">${jaQueKiCards[i]} <img src="images/suits/${cardSuits[k]}.svg" alt="card number and suit"></div>
+    </div>`)
+    }
+}
+
+document.write(`<div class="cards_field">${cards.join("")}</div>`);
